5e6716a2b84edb3115ff6a787df04f47d82a56e2
김민선 <alstjs4739@kookmin.ac.kr>
2023-03-04 14:48:26 +0900

    feat: 전체 페이지 스타일 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
-import Login from "./Pages/Login/Login.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./Pages/Login/Login";
+import Join from "./Pages/Join/Join";
+
 
 function App() {
-    return (
-        <div
-            className="App"
-            style={{
-                maxWidth: "480px",
-                margin: "0 auto",
-                minHeight: "100%",
-                width: "auto",
-                position: "relative",
-                wordBreak: "keep-all",
-            }}
-        >
-            <Login />
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/join" component={<Join />} />
+        </Routes>        
+      </div>
+    </BrowserRouter> 
+  );
 }
 
 export default App;
