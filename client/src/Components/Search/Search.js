fde0275f60ae69f50f7ddb6044a9aa9b063f014b
김민선 <alstjs4739@kookmin.ac.kr>
2023-03-06 23:54:29 +0900

    docs: 파일명 수정

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -1,12 +1,12 @@
 import React from "react";
-import * as style from "./style";
+import * as style from "./styles";
 
 export default function Search() {
     
     return (
         <style.Wrap>
             <style.SearchBlock>
-                <img src={process.env.PUBLIC_URL + "/Images/Search/Search.svg"} />
+                <img src={process.env.PUBLIC_URL + "/Images/Search/SearchIcon.svg"} />
                 <span>단어를 검색해보세요</span>
             </style.SearchBlock>
         </style.Wrap>
