8a5d294ab9a208009113db38b881470fa5017a59
김민선 <alstjs4739@kookmin.ac.kr>
2023-03-19 00:54:16 +0900

    Revert "feat: 메인페이지 버튼 추가, 함수 기능 구현"
    This reverts commit 90cdf51b8af66dfe1440799237ee825709cc430b.

--- client/src/Pages/Class/ClassRegister/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Wrap = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding: 20px 20px 0;
-`;
--- client/src/Pages/Main/styles.js
+++ client/src/Pages/Main/styles.js
@@ -3,3 +3,12 @@ import styled from "styled-components";
 export const Wrap = styled.div`
     padding: 20px 20px 0;
 `;
+
+export const TitleBlock = styled.div`
+    width: 100%;
+    margin-top: 40px;
+    padding: 20px 0 20px 0;
+    border-top: 1.5px solid #E0E0E0;
+    font-weight: 700;
+    font-size: 40px;
+`
