e0fc9d0e63edf19241931965636aaf10ce743dd6
김민선 <alstjs4739@kookmin.ac.kr>
2023-03-25 19:45:03 +0900

    feat: 특이사항 내용 컴포넌트 구현

---
+++ client/src/Components/NoteItem/NoteItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import * as style from "./styles";
+
+export default function NoteItem(props) {
+  return (
+    <style.Wrap>
+      <img src={props.src} />
+      <style.RightBlock>
+        <style.TopBlock>
+          <style.NameBlock>
+            <span>{props.guardianName  + " 보호자"}</span>
+            <span>{props.className}</span>
+          </style.NameBlock>
+          <span>{props.date || "2023.03.25"}</span>
+        </style.TopBlock>
+        <span>{props.title}</span>
+      </style.RightBlock>
+    </style.Wrap>
+  );
+}
