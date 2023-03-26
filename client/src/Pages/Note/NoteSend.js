4e36f8261de48b208a40831f7e508321c139eadf
solhee-hwang <solhee0912@kookmin.ac.kr>
2023-03-26 16:20:53 +0900

    feat : 통합

--- client/src/Pages/Note/NoteSend.js
+++ client/src/Pages/Note/NoteSend.js
@@ -33,48 +33,78 @@ export default function NoteSend() {
     setNoteContent(e.target.value);
   };
 
+  const clickNoteButton = () => {
+    axios
+      .post(
+        "http://localhost:8080/significant/send",
+        JSON.stringify({
+          guardianNickName: guardianName,
+          tutorNickName: sessionStorage.getItem("nickName"),
+          seniorName: seniorName,
+          title: noteTitle,
+          lectureName: className,
+          content: noteContent,
+        }),
+        {
+          headers: { "Content-Type": `application/json` },
+        }
+      )
+      .then(function (resp) {
+        console.log(resp.data);
+        if (resp.data !== null && resp.data !== "") {
+          alert("특이사항을 전송하시겠습니까?");
+          navigate("/noteList");
+        } else {
+          alert("실패");
+        }
+      })
+      .catch(function (err) {
+        console.log(`Error Message: ${err}`);
+      });
+  };
+
   return (
-      <div>
-        <Header title={title} />
-        <style.Wrap>
-          <Input
-            title={"보호자"}
-            border={"#FF7F00"}
-            titleWeight={"500"}
-            titleSize={"23px"}
-            onChange={handleGuardianName}
-          />
-          <Input
-            title={"어르신"}
-            border={"#FF7F00"}
-            titleWeight={"500"}
-            titleSize={"23px"}
-            onChange={handleSeniorName}
-          />
-          <Input
-            title={"클래스 이름"}
-            border={"#FF7F00"}
-            titleWeight={"500"}
-            titleSize={"23px"}
-            onChange={handleClassName}
-          />
-          <Input
-            title={"제목"}
-            border={"#FF7F00"}
-            titleWeight={"500"}
-            titleSize={"23px"}
-            onChange={handleNoteTitle}
-          />
-          <Input
-            height={"150px"}
-            title={"특이사항 작성"}
-            border={"#FF7F00"}
-            titleWeight={"500"}
-            titleSize={"23px"}
-            onChange={handleNoteContent}
-          />
-        </style.Wrap>
-        <Footer title={title} />
-      </div>
+    <div>
+      <Header title={title} onClick={clickNoteButton} />
+      <style.Wrap>
+        <Input
+          title={"보호자"}
+          border={"#FF7F00"}
+          titleWeight={"500"}
+          titleSize={"23px"}
+          onChange={handleGuardianName}
+        />
+        <Input
+          title={"어르신"}
+          border={"#FF7F00"}
+          titleWeight={"500"}
+          titleSize={"23px"}
+          onChange={handleSeniorName}
+        />
+        <Input
+          title={"클래스 이름"}
+          border={"#FF7F00"}
+          titleWeight={"500"}
+          titleSize={"23px"}
+          onChange={handleClassName}
+        />
+        <Input
+          title={"제목"}
+          border={"#FF7F00"}
+          titleWeight={"500"}
+          titleSize={"23px"}
+          onChange={handleNoteTitle}
+        />
+        <Input
+          height={"150px"}
+          title={"특이사항 작성"}
+          border={"#FF7F00"}
+          titleWeight={"500"}
+          titleSize={"23px"}
+          onChange={handleNoteContent}
+        />
+      </style.Wrap>
+      <Footer title={title} />
+    </div>
   );
 }
