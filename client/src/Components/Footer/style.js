aa83216e48e9847b9b8744469f1322498f7bbe71
김민선 <alstjs4739@kookmin.ac.kr>
2023-04-01 04:04:30 +0900

    design: 전체 화면 비율 수정

--- client/src/Components/Footer/style.js
+++ client/src/Components/Footer/style.js
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
 export const Wrap = styled.div`
-  position: sticky;
+  position: absolute;
   width: 100%;
-  max-width: 480px;
+  display: flex;
   bottom: 0;
   background-color: #ffffff;
 `;
 
 export const Menu = styled.div`
-  max-width: 480px;
+  width: 100%;
   height: 60px;
   display: grid;
   align-items: center;
