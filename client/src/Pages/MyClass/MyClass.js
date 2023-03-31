a321a5aa58beeda0767620683aad2abca2b88945
solhee-hwang <solhee0912@kookmin.ac.kr>
2023-03-31 09:16:42 +0900

    feat : 수강색 목록 api 통합

--- client/src/Pages/MyClass/MyClass.js
+++ client/src/Pages/MyClass/MyClass.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import * as style from "./styles";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
@@ -11,7 +12,9 @@ import YellowFullButton from "../../Components/Button/YellowFullButton";
 export default function MyClass() {
   const title = "내 수업";
   const identity = sessionStorage.getItem("identity");
+  const userId = sessionStorage.getItem("userId");
   const navigate = useNavigate();
+  const [adata, setData] = useState([]);
 
   const stateOptions = [
     {
@@ -23,27 +26,69 @@ export default function MyClass() {
       label: "진행완료 수업",
     },
   ];
-
+  useEffect(() => {
+    const url = `http://localhost:8080/tutor-lecture/my-lecture?tutorId=1&state=registered`;
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+  const objArray = adata?.map((item) => ({
+    key: item.lectureId,
+    src: item.imageUrl,
+    className: item.title,
+    classDays: item.monday,
+    classTime: item.activityTime,
+    classExplain: item.lectureIntro,
+    distance: item.distance,
+    location: item.location,
+    starCount: item.likeCount,
+    registerCount: `${item.presentPeople}/${item.maxPeople}`,
+  }));
+  const obj = {
+    data: objArray,
+  };
   return (
     <div>
       <Header title={title} />
       <style.Wrap>
         {identity != 0 && <FloatingButton />}
-          <Dropdown
-            width={"280px"}
-            options={stateOptions}
-            placeholder={"진행중인 수업"}
-          />
-        <ClassCard />
-        <style.ButtonBlock>
-          <YellowFullButton
-            width={"170px"}
-            btnName={"수강생 목록 보기"}
-            onClick={() => {
-              navigate("../studentList")
-            }}
-          />
-        </style.ButtonBlock>
+        <Dropdown
+          width={"280px"}
+          options={stateOptions}
+          placeholder={"진행중인 수업"}
+        />
+        {obj.data.map((item) => {
+          return (
+            <div>
+              <ClassCard
+                key={item.key}
+                src={item.src}
+                className={item.className}
+                classDays={item.classDays}
+                classTime={item.classTime}
+                classExplain={item.classExplain}
+                location={item.location}
+                starCount={item.starCount}
+                registerCount={item.registerCount}
+              />
+              <style.ButtonBlock>
+                <YellowFullButton
+                  width={"170px"}
+                  btnName={"수강생 목록 보기"}
+                  onClick={() => {
+                    navigate("../studentList");
+                  }}
+                />
+              </style.ButtonBlock>
+            </div>
+          );
+        })}
+        ;
       </style.Wrap>
       <Footer title={title} />
     </div>
