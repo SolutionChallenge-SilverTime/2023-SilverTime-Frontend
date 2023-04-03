549661e5f9672136b5cbc5eb500f67159fa592a0
김민선 <alstjs4739@kookmin.ac.kr>
2023-04-03 22:17:05 +0900

    fix: 챗봇 컴포넌트 렌더링 수정

--- client/src/Pages/Chatbot/Chatbot.js
+++ client/src/Pages/Chatbot/Chatbot.js
@@ -3,22 +3,26 @@ import * as style from "./styles";
 
 export default function Chatbot() {
   useEffect(() => {
-    (function(d, m){
+    (function (d, m) {
       const kommunicateSettings = {
-        "appId": "c4414d1878a1fcb5e4cf8473ff06680f",
-        "popupWidget": true,
-        "automaticChatOpenOnNavigation": true,
+        appId: "c4414d1878a1fcb5e4cf8473ff06680f",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
       };
+      if (
+        !document.querySelector(
+          'script[src="https://widget.kommunicate.io/v2/kommunicate.app"]'
+        )
+      ) {
+        const s = document.createElement("script");
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        const h = document.getElementsByTagName("head")[0];
+        h.appendChild(s);
+      }
 
-      const s = document.createElement("script"); 
-      s.type = "text/javascript"; 
-      s.async = true;
-      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-
-      const h = document.getElementsByTagName("head")[0]; 
-      h.appendChild(s);
-
-      window.kommunicate = m; 
+      window.kommunicate = m;
       m._globals = kommunicateSettings;
     })(document, window.kommunicate || {});
   }, []);
